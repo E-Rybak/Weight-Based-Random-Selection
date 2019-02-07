5dcd1f251abe369d9f7f7053e8281cfb63be882d
Emanuel Rybak <manolis123@live.dk>
2019-02-07 13:28:52 +0100

    Add project files.

---
+++ DualUtilityReasoning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DualUtilityReasoning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
